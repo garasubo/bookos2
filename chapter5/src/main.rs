561368b095ac11c36ae83bcb16f1ce7ca89158f7
garasubo <garasubo@gmail.com>
2022-01-09 17:37:56 +0900

    confirm chapter 5 works

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -9,16 +9,13 @@ use cortex_m_semihosting::hprintln;
 use core::mem::MaybeUninit;
 
 mod process;
+mod linked_list;
+mod scheduler;
 mod systick;
 
-use process::ContextFrame;
-
-#[repr(align(8))]
-struct AlignedStack(MaybeUninit<[u8; 1024]>);
-
-extern "C" {
-    fn asm_execute_process(sp: usize);
-}
+use process::{AlignedStack, Process};
+use linked_list::ListItem;
+use scheduler::Scheduler;
 
 #[no_mangle]
 pub unsafe extern "C" fn Reset() -> ! {
@@ -47,28 +44,46 @@ pub unsafe extern "C" fn Reset() -> ! {
 
     #[link_section = ".app_stack"]
     static mut APP_STACK: AlignedStack = AlignedStack(MaybeUninit::uninit());
+    #[link_section = ".app_stack"]
+    static mut APP_STACK2: AlignedStack = AlignedStack(MaybeUninit::uninit());
+    #[link_section = ".app_stack"]
+    static mut APP_STACK3: AlignedStack = AlignedStack(MaybeUninit::uninit());
 
-    let ptr = (APP_STACK.0.as_ptr() as usize) + 1024 - 0x20;
-    let context_frame: &mut ContextFrame = &mut *(ptr as *mut ContextFrame);
+    let process1 = Process::new(&mut APP_STACK, app_main);
+    let mut item1 = ListItem::new(process1);
+    let process2 = Process::new(&mut APP_STACK2, app_main2);
+    let mut item2 = ListItem::new(process2);
+    let process3 = Process::new(&mut APP_STACK3, app_main3);
+    let mut item3 = ListItem::new(process3);
+    let mut sched = Scheduler::new();
+    sched.push(&mut item1);
+    sched.push(&mut item2);
+    sched.push(&mut item3);
 
-    context_frame.r0 = 0;
-    context_frame.r1 = 0;
-    context_frame.r2 = 0;
-    context_frame.r3 = 0;
-    context_frame.r12 = 0;
-    context_frame.lr = 0;
-    context_frame.return_addr = app_main as u32;
-    context_frame.xpsr = 0x0100_0000;
-
-    asm_execute_process(ptr);
-
-    loop {}
+    sched.exec();
 }
 
 extern "C" fn app_main() -> ! {
-    hprintln!("App").unwrap();
-    unsafe { asm!("svc 0"); }
-    loop {}
+    let mut i = 0;
+    loop {
+        hprintln!("App: {}", i).unwrap();
+        unsafe { asm!("svc 0"); }
+        i += 1;
+    }
+}
+
+extern "C" fn app_main2() -> ! {
+    loop {
+        hprintln!("App2").unwrap();
+        unsafe { asm!("svc 0"); }
+    }
+}
+
+extern "C" fn app_main3() -> ! {
+    loop {
+        hprintln!("App3").unwrap();
+        unsafe { asm!("svc 0"); }
+    }
 }
 
 pub union Vector {
